27a59b3a80afd57252887a6de5a4595cfa55583e
toe-evangalist <108993155+toe-evangalist@users.noreply.github.com>
2023-03-07 13:30:43 -0700

    update tf

--- providers.tf
+++ providers.tf
@@ -1,4 +1,14 @@
+# Primarily the AWS provider will be used - I've added in the Prisma Cloud provider as a possiblility for the future
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~>4.0"
+    }
+  }
+}
+
 provider "aws" {
-  version = "~> 4.0"
-  region  = "us-east-1"
+  region = var.aws_region
 }
